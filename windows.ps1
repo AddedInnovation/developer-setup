42d9b127c12afc40b14f82c432307f0c5adebbe7
Added Innovation <dschlum@addedinnovation.com>
2017-12-20 16:18:40 -0600

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -180,12 +180,11 @@ function Install-CoreApps {
     choco install lastpass		--limitoutput --ignorechecksum
     choco install filezilla		--limitoutput        
     choco install ditto			--limitoutput
-    choco install greenshot		--limitoutput    
     choco install ccleaner		--limitoutput
     choco install bluejeansapp		--limitoutput
     choco install microsoft-teams	--limitoutput
     choco install dropbox		--limitoutput
-    
+    choco install greenshot		--limitoutput    
     #trend micro TBD
     #choco install openvpn		--limitoutput
 }
