2983ad275aff0e270d94a3648b68074d2f9c091d
Mark Whisler <mwhisler@addedinnovation.com>
2019-03-05 11:31:21 -0600

    Update Fiddler
    Fiddler installs for the local user by default in AppData\Programs.  Changing to install to Program Files\Telerik\Fiddler so all users can access it.  So dumb.

--- windows.ps1
+++ windows.ps1
@@ -204,7 +204,7 @@ function Install-CoreDevApps {
     choco install dotnetcore-sdk    		--limitoutput
     choco install nuget.commandline		--limitoutput        
     choco install nugetpackageexplorer		--limitoutput    
-    choco install fiddler            		--limitoutput
+    choco install fiddler --install-arguments "/D=C:\Program Files\Telerik\Fiddler"	--limitoutput
     choco install postman			--limitoutput    
     choco install filezilla			--limitoutput
     choco install dotpeek			--limitoutput
