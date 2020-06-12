a573cec145584f61f47c0640e39729049afe7aad
Mark Whisler <mwhisler@addedinnovation.com>
2020-06-12 10:35:55 -0500

    Update dev.ps1

--- dev.ps1
+++ dev.ps1
@@ -3,13 +3,13 @@
 #	Author: Mark Whisler
 #	Company: Added Innovation
 # 
-#	Installs core company and development tools for a .NET developer and VS enterprise subscriber
+#	Installs core company and development tools for a .NET developer and VS Enterprise subscriber
 #
 #
 #	Big thanks to https://github.com/microsoft/windows-dev-box-setup-scripts
 #
 
-#Disable-UAC
+Disable-UAC
 $ConfirmPreference = "None" #ensure installing PowerShell modules don't prompt on needed dependencies
 
 # Get the base URI path from the ScriptToCall value
@@ -33,16 +33,16 @@ function ExecuteScript {
 #	
 #	Set up Windows
 #
-#ExecuteScript "FileExplorerSettings.ps1";
-#ExecuteScript "RemoveDefaultApps.ps1";
+ExecuteScript "FileExplorerSettings.ps1";
+ExecuteScript "RemoveDefaultApps.ps1";
 
 #
 #	Set up Added Core
 #
-#ExecuteScript "AddedCoreApps.ps1";
-#ExecuteScript "AddedCoreDev.ps1";
+ExecuteScript "AddedCoreApps.ps1";
+ExecuteScript "AddedCoreDev.ps1";
 
-#Update-SessionEnvironment
+Update-SessionEnvironment
 
 #ExecuteScript "VisualStudio.ps1"
 
@@ -54,6 +54,6 @@ function ExecuteScript {
 #
 #	Resume and Install Updates
 #
-#Enable-UAC
-#Enable-MicrosoftUpdate
-#Install-WindowsUpdate -acceptEula
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
