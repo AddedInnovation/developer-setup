579251dc554a43fa789697ef1a46cfc7a122d1b7
Mark Whisler <mwhisler@addedinnovation.com>
2022-11-02 12:06:52 -0500

    Updated Visual Studio installations
    Removed all 2019 install components and replaced with 2022 and VS Code.

--- scripts/VisualStudio.ps1
+++ scripts/VisualStudio.ps1
@@ -1,19 +1,16 @@
 
 #
-#	Visual Studio 2019 Enterprise
+#	Visual Studio 2022 Enterprise
 #
-choco install visualstudio2019enterprise				
-choco install visualstudio2019-workload-netcoretools	
-choco install visualstudio2019-workload-manageddesktop	
-choco install visualstudio2019-workload-netweb			
-choco install visualstudio2019-workload-visualstudioextension
-choco install visualstudio2019-workload-azure
-
-#
-#	TODO Add all recommended extensions for VS 2019
-#
-#choco install chocolatey-visualstudio.extension
+choco install visualstudio2022enterprise
+choco install visualstudio2022-workload-manageddesktop	
+choco install visualstudio2022-workload-netweb			
+choco install visualstudio2022-workload-visualstudioextension
+choco install visualstudio20229-workload-azure
 
 Update-SessionEnvironment
 
-#Install-ChocolateyVsixPackage "PowerShellTools" https://marketplace.visualstudio.com/_apis/public/gallery/publishers/AdamRDriscoll/vsextensions/PowerShellToolsforVisualStudio2017-18561/4.9.1/vspackage
+#
+# VS Code
+#
+choco install vscode
