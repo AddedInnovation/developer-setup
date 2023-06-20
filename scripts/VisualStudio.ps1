1fc00079bc193758a72e22c6b0b1b58f8be04d09
Mark Whisler <mwhisler@addedinnovation.com>
2023-06-20 13:58:46 -0500

    Update VisualStudio.ps1
    We need the extension for reporting services projects.

--- scripts/VisualStudio.ps1
+++ scripts/VisualStudio.ps1
@@ -9,6 +9,8 @@ choco install visualstudio2022-workload-visualstudioextension
 choco install visualstudio2022-workload-azure
 choco install visualstudio2022-workload-data
 
+Install-ChocolateyVsixPackage "MicrosoftReportProjectsforVisualStudio2022" "https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ProBITools/vsextensions/MicrosoftReportProjectsforVisualStudio2022/3.0.7/vspackage"
+
 Update-SessionEnvironment
 
 #
